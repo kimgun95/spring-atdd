d0cd27e40ee466e88ec1f85c31cfd1a41219f1ea
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-22 13:23:21 +0900

    add Entity, Id annotation and etc

--- src/main/java/com/example/membership/entity/Membership.java
+++ src/main/java/com/example/membership/entity/Membership.java
@@ -1,4 +1,13 @@
 package com.example.membership.entity;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@NoArgsConstructor
 public class Membership {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
