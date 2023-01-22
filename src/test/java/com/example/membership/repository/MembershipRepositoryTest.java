4c32aa3f265ac7aec5e195f866650c190b561891
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-22 13:41:20 +0900

    make 멤버십등록 test

--- src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.membership.repository;
 
+import com.example.membership.entity.Membership;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,5 +13,20 @@ public class MembershipRepositoryTest {
     @Autowired
     private MembershipRepository membershipRepository;
 
-
+    @Test
+    public void 멤버십등록() {
+        //given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+        //when
+        final Membership result = membershipRepository.save(membership);
+        //then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getPoint()).isEqualTo(10000);
+    }
 }
