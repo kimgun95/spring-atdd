244883ce8e57c3a536dd0be88771c32418d0b1bb
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-22 14:04:07 +0900

    멤버십존재하는지테스트

--- src/main/java/com/example/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/membership/repository/MembershipRepository.java
@@ -1,8 +1,10 @@
 package com.example.membership.repository;
 
 import com.example.membership.entity.Membership;
+import com.example.membership.entity.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
+    Membership findByUserIdAndMembershipType(String userId, MembershipType membershipType);
 }
