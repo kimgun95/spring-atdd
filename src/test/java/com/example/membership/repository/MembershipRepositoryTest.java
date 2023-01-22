1476a7fd8e9b424c03650afffa68ba3a6f01f8ca
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-22 13:22:29 +0900

    add DataJpaTest Annotation

--- src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
@@ -2,9 +2,11 @@ package com.example.membership.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
 public class MembershipRepositoryTest {
 
     @Autowired
