1ae56c2688b934b445f5e33cfdb4f0785af328d1
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 17:26:52 +0900

    make it pass: mockMvc 초기화 및 컨트롤러 생성

--- src/test/java/com/example/membership/controller/MembershipControllerTest.java
+++ src/test/java/com/example/membership/controller/MembershipControllerTest.java
@@ -3,8 +3,10 @@ package com.example.membership.controller;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,10 +15,13 @@ public class MembershipControllerTest {
 
     private MockMvc mockMvc;
     private Gson gson;
+    @InjectMocks
     private MembershipController target;
 
     @Test
     public void mockMvc는Null아님() {
+        mockMvc = MockMvcBuilders.standaloneSetup(target)
+                .build();
         assertThat(target).isNotNull();
         assertThat(mockMvc).isNotNull();
     }
