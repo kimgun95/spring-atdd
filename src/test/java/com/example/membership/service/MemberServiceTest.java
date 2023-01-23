d2daa78b7a433bca880836e8b26efd7cb8463687
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 16:10:49 +0900

    멤버십등록성공 테스트 추가

--- src/test/java/com/example/membership/service/MemberServiceTest.java
+++ src/test/java/com/example/membership/service/MemberServiceTest.java
@@ -14,7 +14,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
@@ -27,14 +28,20 @@ public class MemberServiceTest {
     private MembershipRepository membershipRepository;
     @InjectMocks
     private MembershipService target;
+    private Membership membership;
 
     @BeforeEach
     void setUp() {
         userId = "userId";
         membershipType = MembershipType.NAVER;
         point = 10000;
+        membership = Membership.builder()
+                .id(-1L)
+                .userId(userId)
+                .point(point)
+                .membershipType(MembershipType.NAVER)
+                .build();
     }
-
     @Test
     public void 멤버십등록실패_이미존재() {
         //given
@@ -44,4 +51,23 @@ public class MemberServiceTest {
         //then
         assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
     }
+
+    @Test
+    public void 멤버십등록성공() {
+        // given
+        doReturn(null).when(membershipRepository).findByUserIdAndMembershipType(userId, membershipType);
+        doReturn(membership).when(membershipRepository).save(any(Membership.class));
+
+        // when
+        final Membership result = target.addMembership(userId, membershipType, point);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getMembershipType()).isEqualTo(MembershipType.NAVER);
+
+        // verify
+        verify(membershipRepository, times(1)).findByUserIdAndMembershipType(userId, membershipType);
+        verify(membershipRepository, times(1)).save(any(Membership.class));
+    }
+
 }
