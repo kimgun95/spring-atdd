f5bd474c4135dfae421bb09da628991d16603151
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 15:53:31 +0900

    addMembership 구현

--- src/main/java/com/example/membership/service/MembershipService.java
+++ src/main/java/com/example/membership/service/MembershipService.java
@@ -2,12 +2,20 @@ package com.example.membership.service;
 
 import com.example.membership.entity.Membership;
 import com.example.membership.entity.MembershipType;
+import com.example.membership.exception.MembershipErrorResult;
+import com.example.membership.exception.MembershipException;
+import com.example.membership.repository.MembershipRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MembershipService {
 
+    private MembershipRepository membershipRepository;
+
     public Membership addMembership(String userId, MembershipType membershipType, Integer point) {
+        Membership result = membershipRepository.findByUserIdAndMembershipType(userId, membershipType);
+        if (result != null)
+            throw new MembershipException(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
         return null;
     }
 }
