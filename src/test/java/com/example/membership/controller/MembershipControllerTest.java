7ee524b6e3de6f74e6527d845e52f909279ad973
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 17:39:20 +0900

    api 개발이 되지 않았기에 404 에러 발생

--- src/test/java/com/example/membership/controller/MembershipControllerTest.java
+++ src/test/java/com/example/membership/controller/MembershipControllerTest.java
@@ -1,15 +1,21 @@
 package com.example.membership.controller;
 
+import com.example.membership.dto.MembershipRequest;
+import com.example.membership.entity.MembershipType;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class MembershipControllerTest {
@@ -23,7 +29,29 @@ public class MembershipControllerTest {
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(target)
                 .build();
+        gson = new Gson();
     }
 
+    @Test
+    public void 멤버십등록실패_사용자식별값이헤더에없음() throws Exception {
+        // given
+        final String url = "/api/v1/memberships";
 
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(gson.toJson(membershipRequest(10000, MembershipType.NAVER)))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    private MembershipRequest membershipRequest(final Integer point, final MembershipType membershipType) {
+        return MembershipRequest.builder()
+                .point(point)
+                .membershipType(membershipType)
+                .build();
+    }
 }
