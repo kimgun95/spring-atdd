1983142f07fc6f65410a5cae014f76a65527051d
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 15:49:59 +0900

    exception, service 클래스 생성

---
+++ src/main/java/com/example/membership/service/MembershipService.java
@@ -0,0 +1,13 @@
+package com.example.membership.service;
+
+import com.example.membership.entity.Membership;
+import com.example.membership.entity.MembershipType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MembershipService {
+
+    public Membership addMembership(String userId, MembershipType membershipType, Integer point) {
+        return null;
+    }
+}
