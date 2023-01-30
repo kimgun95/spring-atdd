d049b9c780fd5b682ee60a4770149598a3254e84
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-30 19:44:11 +0900

    service계층: 멤버십상세조회실패 테스트,getMembership 구현

--- src/test/java/com/example/membership/service/MemberServiceTest.java
+++ src/test/java/com/example/membership/service/MemberServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,10 +29,12 @@ public class MemberServiceTest {
     private final String userId = "userId";
     private final MembershipType membershipType = MembershipType.NAVER;
     private final Integer point = 10000;
+    private final Long membershipId = -1L;
     @Mock
     private MembershipRepository membershipRepository;
     @InjectMocks
     private MembershipService target;
+
     private Membership membership() {
         return Membership.builder()
                 .id(-1L)
@@ -78,5 +81,16 @@ public class MemberServiceTest {
         final List<MembershipDetailResponse> result = target.getMembershipList(userId);
         //then
         assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void 멤버십상세조회실패_존재하지않음() {
+        //given
+        doReturn(Optional.empty()).when(membershipRepository).findById(membershipId);
+        //when
+        final MembershipException result = assertThrows(MembershipException.class,
+                () -> target.getMembership(membershipId, userId));
+        //then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.MEMBERSHIP_NOT_FOUND);
     }
 }
