69c3c8ff054a77d6d9a562a041ea98220267ab34
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-31 16:01:33 +0900

    repository계층: 멤버십삭제테스트

--- src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.membership.repository;
 
 import com.example.membership.entity.Membership;
 import com.example.membership.entity.MembershipType;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,14 +16,17 @@ public class MembershipRepositoryTest {
 
     @Autowired
     private MembershipRepository membershipRepository;
+    private static Membership getMembership(MembershipType membershipType, String userId, int point) {
+        return Membership.builder()
+                .userId(userId)
+                .membershipType(membershipType)
+                .point(point)
+                .build();
+    }
     @Test
     public void 멤버십등록() {
         //given
-        final Membership membership = Membership.builder()
-                .userId("userId")
-                .membershipType(MembershipType.NAVER)
-                .point(10000)
-                .build();
+        final Membership membership = getMembership(MembershipType.NAVER, "userId", 10000);
         //when
         final Membership result = membershipRepository.save(membership);
         //then
@@ -34,11 +38,7 @@ public class MembershipRepositoryTest {
     @Test
     public void 멤버십이존재하는지테스트() {
         //given
-        final Membership membership = Membership.builder()
-                .userId("userId")
-                .membershipType(MembershipType.NAVER)
-                .point(10000)
-                .build();
+        final Membership membership = getMembership(MembershipType.NAVER, "userId", 10000);
         //when
         membershipRepository.save(membership);
         final Membership findResult = membershipRepository.findByUserIdAndMembershipType("userId", MembershipType.NAVER);
@@ -59,21 +59,11 @@ public class MembershipRepositoryTest {
         //then
         assertThat(result.size()).isEqualTo(0);
     }
-
     @Test
     public void 멤버십조회_사이즈가2() {
         //given
-        final Membership naverMembership = Membership.builder()
-                .userId("userId")
-                .membershipType(MembershipType.NAVER)
-                .point(10000)
-                .build();
-
-        final Membership kakaoMembership = Membership.builder()
-                .userId("userId")
-                .membershipType(MembershipType.KAKAO)
-                .point(10000)
-                .build();
+        final Membership naverMembership = getMembership(MembershipType.NAVER, "userId", 10000);
+        final Membership kakaoMembership = getMembership(MembershipType.KAKAO, "userId", 10000);
 
         membershipRepository.save(naverMembership);
         membershipRepository.save(kakaoMembership);
@@ -81,5 +71,14 @@ public class MembershipRepositoryTest {
         List<Membership> result = membershipRepository.findAllByuserId("userId");
         //then
         assertThat(result.size()).isEqualTo(2);
+    }
+    @Test
+    public void 멤버십추가후삭제() {
+        //given
+        final Membership membership = getMembership(MembershipType.NAVER, "userId", 10000);
+        final Membership savedMembership = membershipRepository.save(membership);
+        //when
+        membershipRepository.deleteById(savedMembership.getId());
+        //then
     }
 }
