2f9d2cd32845c19898005fde466fa73aa7c4a9a6
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-31 18:35:37 +0900

    pointService 인터페이스 생성, 변경에 유연한 설계

---
+++ src/main/java/com/example/point/service/PointService.java
@@ -0,0 +1,6 @@
+package com.example.point.service;
+
+public interface PointService {
+
+    int calculateAmount(final int price);
+}
--- src/main/java/com/example/point/service/RatePointService.java
+++ src/main/java/com/example/point/service/RatePointService.java
@@ -3,9 +3,13 @@ package com.example.point.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RatePointService {
+public class RatePointService implements PointService {
 
+
+    private static final int POINT_RATE = 1;
+
+    @Override
     public int calculateAmount(final int price) {
-        return price * 1 / 100;
+        return price * POINT_RATE / 100;
     }
 }
